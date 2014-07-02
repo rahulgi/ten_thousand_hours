5a83f2c54093939a9bb220ae93af1f02c8e529ce
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-07-02 10:10:25 +0900

    Initial configuration

--- ten_thousand_hours/settings.py
+++ ten_thousand_hours/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'api',
+    'web',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
