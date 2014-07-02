1edb1c720821b9c1354c90fae70b83a7cfd114a9
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-07-02 11:16:11 +0900

    Added Skill model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Skill(models.Model):
+  """
+  """
+  name = models.CharField(max_length=40)
+  time = models.PositiveIntegerField(default=0)
+  user = models.ForeignKey(User)
