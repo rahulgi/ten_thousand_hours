35986441a75fa0b208475cf6ea647b736577ba0d
Matt Auerbach <tennisfan0526@gmail.com>
2015-06-08 17:03:18 -0700

    added ability to logout

--- web/urls.py
+++ web/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
   url('^skills/(?P<pk>[0-9]+)/$', 'web.views.skill'),
   url('^skills/(?P<pk>[0-9]+)/add_time/$', 'web.views.add_skill_time'),
   url('^create_user/$', 'web.views.register', name='create-new-user'),
+  url('^logout/$', 'web.views.logout_view', name='logout'),
 )
