0d1974fc33f3b2e9a714f2daa1d0363e4df43874
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2015-06-27 16:51:01 -0700

    Basic stuff for time graphing

--- api/views.py
+++ api/views.py
@@ -1,15 +1,21 @@
 from api.models import Skill
 from django.core import serializers
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
 
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET'])
 def skills(request):
   """
   """
   if request.method == 'GET':
     return HttpResponse(serializers.serialize('json', Skill.objects.all()),
                         content_type='application/json')
-  else:
-    pass
+
+@require_http_methods(['GET'])
+def skill_times(request, pk):
+    skill = get_object_or_404(Skill, pk=pk)
+    return HttpResponse(serializers.serialize('json',
+                        skill.timechunk_set.all()),
+                        content_type='application/json')
+
