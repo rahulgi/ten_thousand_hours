5d0e29d0c70ea37af82fb323d871d4b5a1991e7d
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2015-09-19 16:22:04 -0700

    Added prod w/ enviro settings set properly.

---
+++ ten_thousand_hours/settings/prod.py
@@ -0,0 +1,16 @@
+import os
+
+from dev import *
+
+STATIC_ROOT = '/var/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '3306'
+    }
+}
